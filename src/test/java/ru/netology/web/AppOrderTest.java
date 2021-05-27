7c3149b5bffc3cfa500cb7aa540eed2a0080e517
Shliskenstickin <deathwalker@mail.ru>
2021-05-27 13:42:13 +0300

    setup web driver manager 2

--- src/test/java/ru/netology/web/AppOrderTest.java
+++ src/test/java/ru/netology/web/AppOrderTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.By.*;
 
@@ -14,17 +13,17 @@ public class AppOrderTest {
 
     @BeforeAll
     static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void SetUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -35,7 +34,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest01() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("Радаев Иван");
         driver.findElement(cssSelector("[type='tel']")).sendKeys("+79990009900");
         driver.findElement(className("checkbox__box")).click();
@@ -46,7 +44,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest02() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("Radaev Ivan");
         driver.findElement(cssSelector("[type='tel']")).sendKeys("+79990009900");
         driver.findElement(className("checkbox__box")).click();
@@ -57,7 +54,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest03() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='tel']")).sendKeys("+79990009900");
         driver.findElement(className("checkbox__box")).click();
         driver.findElement(className("button__text")).click();
@@ -67,7 +63,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest04() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("Иван");
         driver.findElement(cssSelector("[type='tel']")).sendKeys("+79990009900");
         driver.findElement(className("checkbox__box")).click();
@@ -78,7 +73,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest05() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("Радаев Иван");
         driver.findElement(className("checkbox__box")).click();
         driver.findElement(className("button__text")).click();
@@ -88,7 +82,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest06() {
-        driver.get("http://localhost:9999");
         driver.findElement(className("button__text")).click();
         assertTrue(!driver.findElements(cssSelector("[class=\"input input_type_text input_view_default input_size_m input_width_available input_has-label input_invalid input_theme_alfa-on-white\"]")).isEmpty(),
                 "Отправка пустой формы");
@@ -96,7 +89,6 @@ public class AppOrderTest {
 
     @Test
     void FormTest07() {
-        driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("Радаев Иван");
         driver.findElement(cssSelector("[type='tel']")).sendKeys("+79990009900");
         driver.findElement(className("button__text")).click();
